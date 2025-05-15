c7122de2dc36a50f77c345e4da79a49b7e894a68
VioNSaibot <agalcev22lol@gmail.com>
2025-05-15 20:48:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -219,6 +219,6 @@ def run():
     asyncio.get_event_loop().run_until_complete(on_startup())
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port
+    app.run(host="0.0.0.0", port=port)
 
 run()
